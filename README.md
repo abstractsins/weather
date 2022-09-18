7791f0f64739fd91ae490bd56840320128078b31
Dan The Man <18626012+abstractsins@users.noreply.github.com>
2022-09-18 17:31:52 -0400

    add web UI

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # weather app
 ## using OpenWeatherMap API
 
-### Currently fetches and logs temperature and humidity for Philadelphia, PA
+
+### versions
+`1.0` Fetches and logs temperature and humidity for Philadelphia, PA...
+
+`1.1` Added capability to describe cloud coverage or other details of the sky, for 3 hour forecast window
+
+`2.0` Added html UI to display forecast
+
+
+### future
+* suggest clothing in general
+* suggest your actual outfits that you entered into database
--- main.js
+++ main.js
@@ -11,10 +11,33 @@ const getWeather = async() => {
     .then(response => response.json())
     .then(data => {
         let main = data["list"][0]["main"];
-        let temp = main["temp"];
+        let weather = data["list"][0]["weather"];
+        
+        let forecastEnd = data["list"][0]["dt_txt"].substring(11,16)
+        if (forecastEnd[0]==='0') forecastEnd=forecastEnd.substring(1)
+        let hour = Number(forecastEnd.substring(0,2));
+        if (hour >= 12) {
+            forecastEnd = hour-12 + forecastEnd.substring(2);
+            forecastEnd += ' p.m.';
+        }
+        else forecastEnd += ' a.m.';
+        
+        let temp = Math.round(main["temp"]*10)/10;
         let humidity = main["humidity"];
-        console.log(`The temperature right now in Philadelphia is ${temp}°F`);
-        console.log(`with a humidity of ${humidity}%`);
+
+        let sky = {};
+        sky.desc = weather[0]["description"].split('');
+        sky.desc = sky.desc.join('')
+        if (sky.desc[sky.desc.length-1]==='s') {
+            sky.isPlural = true;
+        } else sky.isPlural = false;
+        if (sky.isPlural) {
+            sky.desc += ' are';
+            sky.desc.splice(0,1,sky.desc[0].toUpperCase())
+        } else sky.desc = 'A ' + sky.desc + ' is';
+
+        console.log(`The temperature right now in Philadelphia is ${temp}°F, with a humidity of ${humidity}%`);
+        console.log(`${sky.desc} expected until ${forecastEnd}`)
 
     })
     .catch(err=>{
